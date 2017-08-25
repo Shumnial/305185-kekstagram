0c723de0bae03081f09eeca0529319850072ad61
Nikita Shumakov <shumakov.n@yandex.ru>
2017-08-25 20:20:42 +0500

    Оптимизиция кода

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,5 @@
 'use strict';
-var comments = [
+var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -8,51 +8,54 @@ var comments = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var getRandomLikes = function (min, max) {
-  return Math.floor(Math.random() * (max + 1 - min) + min);
+var PHOTO_ELEMENTS = [];
+
+var getRandomInteger = function (min, max) {
+  Math.floor(Math.random() * (max + 1 - min) + min);
 };
 
+var getRandomArrayElement = function (array) {
+  return array[getRandomInteger(0, array.length)];
+};
 
 var getRandomComments = function (array) {
   var commentsNumber = [];
-  var firstComment = array[Math.floor(Math.random() * array.length)];
+  var firstComment = getRandomArrayElement(COMMENTS);
   commentsNumber[0] = firstComment;
-  var oneOrTwo = Math.floor(1 + Math.random() * 2);
+  var oneOrTwo = getRandomInteger(1, 2);
   if (oneOrTwo === 2) {
-    var secondComment = array[Math.floor(Math.random() * array.length)];
+    var secondComment = getRandomArrayElement(COMMENTS);
     while (secondComment === firstComment) {
-      secondComment = array[Math.floor(Math.random() * array.length)];
+      secondComment = getRandomArrayElement(COMMENTS);
     }
     commentsNumber[1] = secondComment;
   }
   return commentsNumber;
 };
 
-var photoElement = [];
-
 for (var i = 0; i <= 24; i++) {
   var photo = {
     url: 'photos/' + (i + 1) + '.jpg',
-    likes: getRandomLikes(15, 200),
-    comments: getRandomComments(comments)
+    likes: getRandomInteger(15, 200),
+    comments: getRandomComments(COMMENTS)
   };
-  photoElement[i] = photo;
+  PHOTO_ELEMENTS[i] = photo;
 }
 
 var getGalleryTemplate = function (array) {
   var fragment = document.createDocumentFragment();
   var template = document.querySelector('#picture-template').content;
-  for (i = 0; i < photoElement.length; i++) {
+  for (i = 0; i < PHOTO_ELEMENTS.length; i++) {
     var element = template.cloneNode(true);
-    element.querySelector('img').setAttribute('src', photoElement[i].url);
-    element.querySelector('.picture-comments').textContent = photoElement[i].comments.length;
-    element.querySelector('.picture-likes').textContent = photoElement[i].likes;
+    element.querySelector('img').setAttribute('src', PHOTO_ELEMENTS[i].url);
+    element.querySelector('.picture-comments').textContent = PHOTO_ELEMENTS[i].comments.length;
+    element.querySelector('.picture-likes').textContent = PHOTO_ELEMENTS[i].likes;
     fragment.appendChild(element);
   }
   document.querySelector('.pictures').appendChild(fragment);
 };
 
-getGalleryTemplate(photoElement);
+getGalleryTemplate(PHOTO_ELEMENTS);
 
 var showHideOverlay = function () {
   var uploadOverlay = document.querySelector('.upload-overlay');
@@ -67,11 +70,11 @@ var getPhotoDetails = function (array) {
   var fragment = document.createDocumentFragment();
   var galleryBlock = document.querySelector('.gallery-overlay-preview');
   var element = galleryBlock.cloneNode(true);
-  element.querySelector('.gallery-overlay-image').setAttribute('src', photoElement[0].url);
-  element.querySelector('.likes-count').textContent = photoElement[0].likes;
-  element.querySelector('.comments-count').textContent = photoElement[0].comments.length;
+  element.querySelector('.gallery-overlay-image').setAttribute('src', PHOTO_ELEMENTS[0].url);
+  element.querySelector('.likes-count').textContent = PHOTO_ELEMENTS[0].likes;
+  element.querySelector('.comments-count').textContent = PHOTO_ELEMENTS[0].comments.length;
   fragment.appendChild(element);
   document.querySelector('.gallery-overlay').replaceChild(fragment, galleryBlock);
 };
 
-getPhotoDetails(photoElement);
+getPhotoDetails(PHOTO_ELEMENTS);
