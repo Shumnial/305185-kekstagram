4e216bd5a576f4377b88784ec3fa41911ea5f32c
Nikita Shumakov <shumakov.n@yandex.ru>
2017-09-18 17:59:45 +0500

    Исправлены ошибки по следующим критериям: Д25

--- js/get-fragment.js
+++ js/get-fragment.js
@@ -7,14 +7,14 @@
   window.getFragment = function (picturesData) {
     var fragment = document.createDocumentFragment();
     var template = document.querySelector('#picture-template').content;
-    for (var i = 0; i < picturesData.length; i++) {
+    picturesData.forEach(function (pictureElement) {
       var element = template.cloneNode(true);
-      element.querySelector('img').setAttribute('src', picturesData[i].url);
-      element.querySelector('.picture-comments').textContent = picturesData[i].comments.length;
-      element.querySelector('.picture-likes').textContent = picturesData[i].likes;
+      element.querySelector('img').setAttribute('src', pictureElement.url);
+      element.querySelector('.picture-comments').textContent = pictureElement.comments.length;
+      element.querySelector('.picture-likes').textContent = pictureElement.likes;
       fragment.appendChild(element);
       filtersContainer.classList.remove('hidden');
-    }
+    });
     return fragment;
   };
 })();
